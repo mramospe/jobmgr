04a432a8e0995039d2a1ce4896ad90c8742926e7
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2018-05-03 13:20:26 +0200

    Add new test function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,3 +34,26 @@ def test_create_dir():
     for i in range(3):
         stepped_job.utils.create_dir(new_dir)
     assert list(sorted(map(int, os.listdir(new_dir)))) == list(range(4))
+
+
+def test_merge_dicts():
+    '''
+    Test for "merge_dicts"
+    '''
+    # Test the merging procedure
+    a = {'a': 1, 'b': 2}
+    b = {'c': 3, 'd': 4}
+    c = {'e': 5, 'f': 6}
+
+    res = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+
+    assert set(stepped_job.utils.merge_dicts(a, b, c).items()) == set(res.items())
+
+    # Test that it overwrites values
+    a = {'a': 1, 'b': 2}
+    b = {'a': 3, 'd': 4}
+    c = {'b': 3, 'c': 4}
+
+    res = {'a': 3, 'b': 3, 'c': 4, 'd': 4}
+
+    assert set(stepped_job.utils.merge_dicts(a, b, c).items()) == set(res.items())
